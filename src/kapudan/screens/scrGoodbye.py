988e4fd8183443015bd7dcafd12771b02a5ef36b
george <rpubaddr0@gmail.com>
2012-05-30 17:53:07 -0600

    added renamed files

---
+++ src/kapudan/screens/scrGoodbye.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012, The Chakra Developers
+#
+# This is a fork of Pardus' Kapudan, which is
+# Copyright (C) 2005-2009, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
+from PyQt4 import QtGui
+from PyQt4.QtCore import *
+from PyKDE4.kdecore import ki18n, KGlobal, KConfig
+
+import subprocess, sys
+
+from kapudan.screen import Screen
+from kapudan.screens.ui_scrGoodbye import Ui_goodbyeWidget
+
+class Widget(QtGui.QWidget, Screen):
+    title = ki18n("More")
+    desc = ki18n("Congratulations!")
+
+    def __init__(self, *args):
+        QtGui.QWidget.__init__(self,None)
+        self.ui = Ui_goodbyeWidget()
+        self.ui.setupUi(self)
+
+        lang = KGlobal.locale().language()
+        if lang in ["de", "el", "es", "it", "uz"]:
+            self.helpPageUrl = "http://chakra-linux.org/wiki/index.php/Help/" + lang
+        else:
+            self.helpPageUrl = "http://chakra-linux.org/wiki/index.php/Help"
+        if lang in ["de", "es", "eu", "fr", "it", "pl", "pt-br", "sv", "ru", "uz", "zh-hant"]:
+            self.beginnersGuideUrl = "http://chakra-linux.org/wiki/index.php/Beginners_Guide/" + lang
+        else:
+            self.beginnersGuideUrl = "http://chakra-linux.org/wiki/index.php/Beginners_Guide"
+
+    def on_buttonSystemSettings_clicked(self):
+        self.procSettings = QProcess()
+        self.procSettings.start("systemsettings")
+
+    def on_buttonHelpPages_clicked(self):
+        self.procSettings = QProcess()
+        command = "kfmclient openURL " + self.helpPageUrl
+        self.procSettings.start(command)
+
+    def on_buttonBeginnersGuide_clicked(self):
+        self.procSettings = QProcess()
+        command = "kfmclient openURL " + self.beginnersGuideUrl
+        self.procSettings.start(command)
+
+    def execute(self):
+       return True
+
---
+++ src/kapudan/tools/tools.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import dbus
+import glob
+import subprocess
+from PyQt4.QtGui import QDesktopWidget
+
+def DBus():
+    if not dbus.get_default_main_loop():
+        from dbus.mainloop.qt import DBusQtMainLoop
+        DBusQtMainLoop(set_as_default = True)
+
+def importScreen(screenName):
+    ''' imports a screen by name '''
+    screen = __import__(screenName)
+    for s in screenName.split('.')[1:]:
+        screen = getattr(screen, s)
+    return screen
+
+def loadScreens(screensPath, globals):
+    ''' imports all screens in the specified directory '''
+    screens = glob.glob(screensPath)
+    for screen in screens:
+        screenName = screen.split("/")[-1].split(".")[0]
+        globals[screenName] = importScreen("kapudan.screens." + screenName)
+
+def isLiveCD():
+    return os.path.exists('/var/run/pardus/livemedia')
+
+def getRelease():
+    p = subprocess.Popen(["lsb_release", "-irs"], stdout=subprocess.PIPE)
+    release, err = p.communicate()
+    return unicode(release.replace("\n", ""))
+
+def killPlasma(self):
+    p = subprocess.Popen(["pidof", "-s", "plasma-desktop"], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    pidOfPlasma = int(out)
+
+    try:
+        os.kill(pidOfPlasma, 15)
+        self.startPlasma()
+    except OSError, e:
+        print 'WARNING: failed os.kill: %s' % e
+        print "Trying SIGKILL"
+        os.kill(pidOfPlasma, 9)
+        startPlasma()
+
+def startPlasma(self):
+    p = subprocess.Popen(["plasma-desktop"], stdout=subprocess.PIPE)
+
+def centerWindow(window):
+    rect   = QDesktopWidget().screenGeometry()
+    width  = 0
+    heigth = 0
+
+    if rect.width <= 640: width = 620
+    elif rect.width <= 800: width = 720
+    else: width = 960
+
+    if rect.height <= 480: height = 450
+    elif rect.height <= 600: height = 520
+    else: height = 680
+
+    window.resize(width, height)
+    window.move(rect.width()/2 - window.width()/2, rect.height()/2 - window.height()/2)
