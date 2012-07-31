2f7a36af45852c5493a7239d11315fe2b3df3220
Fabian Kosmale <0inkane@googlemail.com>
2012-07-31 12:12:17 +0200

    replaced grep call with python

--- src/kapudan/tools/rcconf.py
+++ src/kapudan/tools/rcconf.py
@@ -3,15 +3,15 @@
 
 import os
 
+
 class RCDaemon():
 
     def isEnabled(self, daemon):
-        state = os.system("grep ^DAEMONS=\( /etc/rc.conf | grep -q \"[( @]" + daemon + "[ )]\"")
-        if state == 0:
-            return True
-        else:
-            return False
+        with open("/etc/rc.conf", "r") as f:
+            for line in f:
+                if line.startswith("DAEMONS=("):
+                #FIXME: work when DAEMONS is split over multiple lines
+                    return daemon in line
 
     def isInstalled(self, daemon):
         return os.path.isfile("/etc/rc.d/" + daemon)
-
