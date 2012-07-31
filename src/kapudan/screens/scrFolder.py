16785a7c5d846cba3bb108d6ae868ef415ceaba8
Fabian Kosmale <0inkane@googlemail.com>
2012-07-31 23:33:16 +0200

    working on the folder screen

--- src/kapudan/screens/scrFolder.py
+++ src/kapudan/screens/scrFolder.py
@@ -13,6 +13,7 @@
 # Please read the COPYING file.
 #
 
+import os
 from PyQt4 import QtGui
 from PyQt4.QtCore import *
 from PyKDE4.kdecore import ki18n
@@ -26,8 +27,8 @@ class Widget(QtGui.QWidget, Screen):
     screenSettings["hasChanged"] = False
 
     # title and description at the top of the dialog window
-    title = ki18n("Mouse")
-    desc = ki18n("Setup Mouse Behavior")
+    title = ki18n("Folder")
+    desc = ki18n("Create folders in the home directory")
 
     def __init__(self, *args):
         QtGui.QWidget.__init__(self, None)
@@ -35,12 +36,31 @@ class Widget(QtGui.QWidget, Screen):
         self.ui.setupUi(self)
 
         # read default settings
+        self.download_path  = os.path.expanduser("~/Downloads")
+        self.documents_path = os.path.expanduser("~/Documents")
+        self.video_path     = os.path.expanduser("~/Video")
+        self.music_path     = os.path.expanduser("~/Music")
+        self.image_path     = os.path.expanduser("~/Images")
+
+        self.download  = os.path.isdir(self.download_path)
+        self.documents = os.path.isdir(self.download_path)
+        self.video     = os.path.isdir(self.download_path)
+        self.music     = os.path.isdir(self.download_path)
+        self.image     = os.path.isdir(self.download_path)
 
         # set signals
-        #self.connect(self.ui.radioButtonRightHand, SIGNAL("toggled(bool)"), self.setHandedness)
-        #self.connect(self.ui.checkReverse, SIGNAL("toggled(bool)"), self.setHandedness)
-        #self.connect(self.ui.singleClick, SIGNAL("clicked()"), self.clickBehaviorToggle)
-        #self.connect(self.ui.DoubleClick, SIGNAL("clicked()"), self.clickBehaviorToggle)
+        self.connect(self.ui.documentsFolderButton, SIGNAL("toggled(bool)"), self.addFolder)
+        self.connect(self.ui.downloadFolderButton, SIGNAL("toggled(bool)"), self.addFolder)
+        self.connect(self.ui.imageFolderButton, SIGNAL("toggled(bool)"), self.addFolder)
+        self.connect(self.ui.musicFolderButton, SIGNAL("toggled(bool)"), self.addFolder)
+        self.connect(self.ui.videoFolderButton, SIGNAL("toggled(bool)"), self.addFolder)
+
+    def addFolder(self, item):
+        self.download  = self.ui.downloadFolderButton.isChecked()
+        self.image     = self.ui.imageFolderButton.isChecked()
+        self.documents = self.ui.documentsFolderButton.isChecked()
+        self.music     = self.ui.musicFolderButton.isChecked()
+        self.video     = self.ui.videoFolderButton.isChecked()
 
     def shown(self):
         pass
