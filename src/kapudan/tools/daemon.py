ec889469e02252271c5bfb59df0b554c95ad50ca
Fabian Kosmale <0inkane@googlemail.com>
2012-10-08 14:20:18 +0200

    added support for systemd

--- src/kapudan/tools/daemon.py
+++ src/kapudan/tools/daemon.py
@@ -1,6 +1,7 @@
-import sys
 import warnings
 import functools
+import re
+import os
 
 
 def deprecated(message=None):
@@ -26,13 +27,39 @@ def deprecated(message=None):
 
 
 class Daemon(object):
-    def __init__(self):
-        print >> sys.stderr, "Unfinished"
+    _matcher = re.compile("(en|dis)abled")
 
-    @deprecated("You shouldn't rely on this method, as it doesn't work with systemd")
+    def __init__(self, name=None):
+        if name is None:
+            warnings.warn_explicit("Using  Daemon this way is deprecated")
+        self.name = name
+
+    @deprecated("You shouldn't rely on this method, but rather use is_enabled")
     def isEnabled(self, name):
+        if self.name:
+            raise Warning("Using the old API, but name is not None! Aborting")
+        self.name = name
+        result = self.is_enabled()
+        self.name = None
+        return result
+
+    @deprecated("You shouldn't rely on this method, but rather use is_installed")
+    def isInstalled(self, name):
+        if self.name:
+            raise Warning("Using the old API, but name is not None! Aborting")
+        self.name = name
+        result = self.is_installed()
+        self.name = None
+        return result
+
+    def is_enabled(self):
+        result = os.popen("systemctl show --property=UnitFileState %s") % self.name
+        match = re.search(Daemon._matcher, result)
+        if match:
+            return "enabled" == match.group()
         return False
 
-    @deprecated("You shouldn't rely on this method, as it doesn't work with systemd")
-    def isInstalled(self, name):
-        return True
+    def is_installed(self):
+        result = os.popen("systemctl show --property=UnitFileState %s") % self.name
+        match = re.search(Daemon._matcher, result)
+        return match is not None
