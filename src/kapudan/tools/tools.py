1d8d97e8cacae0376500b344613c36a5c09af31b
Fabian Kosmale <0inkane@googlemail.com>
2013-03-01 22:06:20 +0100

    PEP8ified tools.py

--- src/kapudan/tools/tools.py
+++ src/kapudan/tools/tools.py
@@ -7,10 +7,12 @@ import glob
 import subprocess
 from PyQt4.QtGui import QDesktopWidget
 
+
 def DBus():
     if not dbus.get_default_main_loop():
         from dbus.mainloop.qt import DBusQtMainLoop
-        DBusQtMainLoop(set_as_default = True)
+        DBusQtMainLoop(set_as_default=True)
+
 
 def importScreen(screenName):
     ''' imports a screen by name '''
@@ -19,6 +21,7 @@ def importScreen(screenName):
         screen = getattr(screen, s)
     return screen
 
+
 def loadScreens(screensPath, globals):
     ''' imports all screens in the specified directory '''
     screens = glob.glob(screensPath)
@@ -26,13 +29,16 @@ def loadScreens(screensPath, globals):
         screenName = screen.split("/")[-1].split(".")[0]
         globals[screenName] = importScreen("kapudan.screens." + screenName)
 
+
 def isLiveCD():
     return os.path.exists('/var/run/pardus/livemedia')
+
 
 def getRelease():
     p = subprocess.Popen(["lsb_release", "-ircs"], stdout=subprocess.PIPE)
     release, err = p.communicate()
     return release.replace("\n", "")
+
 
 def killPlasma(self):
     p = subprocess.Popen(["pidof", "-s", "plasma-desktop"], stdout=subprocess.PIPE)
@@ -48,21 +54,29 @@ def killPlasma(self):
         os.kill(pidOfPlasma, 9)
         startPlasma()
 
+
 def startPlasma(self):
-    p = subprocess.Popen(["plasma-desktop"], stdout=subprocess.PIPE)
+    subprocess.Popen(["plasma-desktop"], stdout=subprocess.PIPE)
+
 
 def centerWindow(window):
-    rect   = QDesktopWidget().screenGeometry()
-    width  = 0
-    heigth = 0
+    rect = QDesktopWidget().screenGeometry()
+    width = 0
+    height = 0
 
-    if rect.width <= 640: width = 620
-    elif rect.width <= 800: width = 720
-    else: width = 960
+    if rect.width <= 640:
+        width = 620
+    elif rect.width <= 800:
+        width = 720
+    else:
+        width = 960
 
-    if rect.height <= 480: height = 450
-    elif rect.height <= 600: height = 520
-    else: height = 680
+    if rect.height <= 480:
+        height = 450
+    elif rect.height <= 600:
+        height = 520
+    else:
+        height = 680
 
     window.resize(width, height)
-    window.move(rect.width()/2 - window.width()/2, rect.height()/2 - window.height()/2)
+    window.move(rect.width() / 2 - window.width() / 2, rect.height() / 2 - window.height() / 2)
