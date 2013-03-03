d35aca09c89567cda238616abb57dc8600bb4f3d
Fabian Kosmale <0inkane@googlemail.com>
2013-03-03 13:01:05 +0100

    working on authentification

--- kauth/daemon_helper.cpp
+++ kauth/daemon_helper.cpp
@@ -1,28 +1,46 @@
 #include <QDBusInterface>
 #include <QDBusConnection>
 #include "daemon_helper.h"
+
 ActionReply DaemonHelper::enabledaemon(QVariantMap args)
 {
+	ActionReply reply;
 	QString daemon = args["daemonname"].toString();
-	QDBusInterface sytemdManager( "org.freedesktop.systemd1", "/Manager",
+	QDBusInterface systemdManager( "org.freedesktop.systemd1", "/Manager",
 		       "org.freedesktop.systemd1.Manager", QDBusConnection::systemBus());
+	if (!systemdManager.isValid()) {
+		reply =  ActionReply::HelperErrorReply;
+		reply.setErrorDescription("invalid interface");
+		reply.setErrorCode(42);
+		return reply;
+	}
 	QList<QVariant> arguments;
 	arguments.append(args["daemonname"]); // name of the daemon
 	arguments.append(false); // enable persistently
 	arguments.append(true); // replace symlinks if necessary
-	systemdManagerInterface->callWithArgumentList("EnableUnitFiles", arguments)
-	return ActionReply::SuccessReply;
+	QDBusMessage msg = systemdManager.callWithArgumentList(QDBus::Block, "EnableUnitFiles", arguments);
+	if (msg.type() != QDBusMessage::ReplyMessage) {
+		reply =  ActionReply::HelperErrorReply;
+		//reply.setErrorDescription(systemdManager.lastError().message());
+		reply.setErrorDescription("we got an error :-(");
+		reply.setErrorCode(43);
+		return reply;
+	}
+	reply = ActionReply::SuccessReply;
+	reply.addData("contents", msg.arguments());
+	reply.addData("number", 1);
+	return reply;
 }
 
 ActionReply DaemonHelper::disabledaemon(QVariantMap args)
 {
 	QString daemon = args["daemonname"].toString();
-	QDBusInterface sytemdManager( "org.freedesktop.systemd1", "/Manager",
+	QDBusInterface systemdManager( "org.freedesktop.systemd1", "/Manager",
 		       "org.freedesktop.systemd1.Manager", QDBusConnection::systemBus());
 	QList<QVariant> arguments;
 	arguments.append(args["daemonname"]); // name of the daemon
 	arguments.append(false); // disable persistently
-	systemdManagerInterface->callWithArgumentList("DisableUnitFiles", arguments)
+	systemdManager.callWithArgumentList(QDBus::Block, "DisableUnitFiles", arguments);
 	return ActionReply::SuccessReply;
 }
 
--- kauth/daemon_helper.h
+++ kauth/daemon_helper.h
@@ -1,15 +1,19 @@
+#ifndef DAEMON_HELPER_H
+#define DAEMON_HELPER_H
+
 #include <kauth.h>
-#include <QDBusInterface.h>
+
+#include <QDBusInterface>
 
 using namespace KAuth;
 
-class DaemonHelper : pulbic QObject
+class DaemonHelper : public QObject
 {
 	Q_OBJECT
 	
 	public slots:
 		ActionReply enabledaemon(QVariantMap args);
 		ActionReply disabledaemon(QVariantMap args);
-	private:
-		QDBusInterface systemdManagerInterface;
-}
+};
+
+#endif
