04eb3736a3a761b356912f98d5fb756cbe042576
Adrián Chaves Fernández (Gallaecio) <adriyetichaves@gmail.com>
2013-08-25 13:00:12 +0200

    Updated the translation template and translations from Transifex.

--- setup.py
+++ setup.py
@@ -62,11 +62,11 @@ def update_messages():
                         --keyword=ki18n \
                         -o po/%s.pot" % (about.catalog, about.catalog))
 
-    # Update PO files
-    for item in os.listdir("po"):
-        if item.endswith(".po"):
-            os.system("msgmerge --no-wrap --sort-by-file -q -o .tmp/temp.po po/%s po/%s.pot" % (item, about.catalog))
-            os.system("cp .tmp/temp.po po/%s" % item)
+    ## Update PO files
+    #for item in os.listdir("po"):
+        #if item.endswith(".po"):
+            #os.system("msgmerge --no-wrap --sort-by-file -q -o .tmp/temp.po po/%s po/%s.pot" % (item, about.catalog))
+            #os.system("cp .tmp/temp.po po/%s" % item)
 
     # Remove temporary directory
     shutil.rmtree(".tmp")
