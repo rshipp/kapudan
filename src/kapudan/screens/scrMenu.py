7211094fada7f0c8801f57b32c9cbe48a577fe43
rshipp <github@rshipp.com>
2016-10-15 15:51:05 -0500

    Fix scrMenu, add Parser class from kaptan5

--- src/kapudan/screens/scrMenu.py
+++ src/kapudan/screens/scrMenu.py
@@ -1,23 +1,15 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) 2012-2015, The Chakra Developers
-#
-# This is a fork of Pardus's Kaptan, which is
-# Copyright (C) 2005-2009, TUBITAK/UEKAE
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free
-# Software Foundation; either version 2 of the License, or (at your option)
-# any later version.
-#
-# Please read the COPYING file.
-#
+import os
 
 from PyQt5 import QtGui, QtWidgets
 from PyQt5.QtCore import QCoreApplication, QSettings
 
 from kapudan.screen import Screen
 from kapudan.screens.ui_scrMenu import Ui_menuWidget
+from kapudan.tools.config import Parser
+
+MENU_KICKOFF = "org.kde.plasma.kickoff"
+MENU_KICKER = "org.kde.plasma.kicker"
+MENU_KICKERDASH = "org.kde.plasma.kickerdash"
 
 
 class Widget(QtWidgets.QWidget, Screen):
@@ -33,111 +25,69 @@ class Widget(QtWidgets.QWidget, Screen):
         self.ui = Ui_menuWidget()
         self.ui.setupUi(self)
 
-        # read default menu style first
-        config = QSettings("plasma-desktop-appletsrc")
-        # FIXME
-        #group = config.group("Containments")
-
         self.menuNames = {}
-        self.menuNames["launcher"] = {
+        self.menuNames[MENU_KICKOFF] = {
             "menuIndex": 0,
             "summaryMessage": QCoreApplication.translate("kapudan", "Kick-off Menu"),
             "image": QtGui.QPixmap(':/raw/pixmap/kickoff.png'),
             "description": QCoreApplication.translate("kapudan", "Kick-off menu is the default menu of Chakra.<br><br>The program shortcuts are easy to access and well organized.")
         }
-        self.menuNames["simplelauncher"] = {
+        self.menuNames[MENU_KICKER] = {
             "menuIndex": 1,
             "summaryMessage": QCoreApplication.translate("kapudan", "Simple Menu"),
             "image": QtGui.QPixmap(':/raw/pixmap/simple.png'),
             "description": QCoreApplication.translate("kapudan", "Simple menu is an old style menu from KDE 3.<br><br>It is a very lightweight menu thus it is recommended for slower PC's.")
         }
-        self.menuNames["lancelot_launcher"] = {
+        self.menuNames[MENU_KICKERDASH] = {
             "menuIndex": 2,
-            "summaryMessage": QCoreApplication.translate("kapudan", "Lancelot Menu"),
-            "image": QtGui.QPixmap(':/raw/pixmap/lancelot.png'),
-            "description": QCoreApplication.translate("kapudan", "Lancelot is an advanced and highly customizable menu for Chakra.<br><br>The program shortcuts are easy to access and well organized.")
-        }
-        self.menuNames["homerunlauncher"] = {
-            "menuIndex": 3,
-            "summaryMessage": QCoreApplication.translate("kapudan", "Homerun Menu"),
-            "image": QtGui.QPixmap(':/raw/pixmap/homerun.png'),
-            "description": QCoreApplication.translate("kapudan", "Homerun is a full screen launcher with content organized in tabs.")
-        }
-        self.menuNames["appmenu-qml"] = {
-            "menuIndex": 4,
-            "summaryMessage": QCoreApplication.translate("kapudan", "AppMenu QML"),
-            "image": QtGui.QPixmap(':/raw/pixmap/appmenu-qml.png'),
-            "description": QCoreApplication.translate("kapudan", "This plasmoid shows a menu of the installed applications, similar to Lancelot but much simpler")
-        }
-        self.menuNames["org.kde.homerun-kicker"] = {
-            "menuIndex": 5,
-            "summaryMessage": QCoreApplication.translate("kapudan", "Homerun Kicker"),
+            "summaryMessage": QCoreApplication.translate("kapudan", "Application menu"),
             "image": QtGui.QPixmap(':/raw/pixmap/homerun-kicker.png'),
-            "description": QCoreApplication.translate("kapudan", "A non-fullscreen version of Homerun.")
+            "description": QCoreApplication.translate("kapudan", "A fullscreen menu.")
         }
 
-        # FIXME:
-        #for each in list(group.groupList()):
-        #    subgroup = group.group(each)
-        #    subcomponent = subgroup.readEntry('plugin')
-        #    if subcomponent == 'panel':
-        #        subg = subgroup.group('Applets')
-        #        for i in list(subg.groupList()):
-        #            subg2 = subg.group(i)
-        #            launcher = subg2.readEntry('plugin')
-        #            if str(launcher).find('launcher') >= 0:
-        #                self.__class__.screenSettings["selectedMenu"] = subg2.readEntry('plugin')
-
-        # set menu preview to default menu
-        # if default menu could not found, default to kickoff
-        if "selectedMenu" not in self.__class__.screenSettings:
-            self.__class__.screenSettings["selectedMenu"] = "launcher"
-
-        self.ui.pictureMenuStyles.setPixmap(self.menuNames[str(self.__class__.screenSettings["selectedMenu"])]["image"])
-        self.ui.labelMenuDescription.setText(self.menuNames[str(self.__class__.screenSettings["selectedMenu"])]["description"])
-        self.ui.menuStyles.setCurrentIndex(self.menuNames[str(self.__class__.screenSettings["selectedMenu"])]["menuIndex"])
+        self.ui.pictureMenuStyles.setPixmap(self.menuNames[self.selectedMenu]["image"])
+        self.ui.labelMenuDescription.setText(self.menuNames[self.selectedMenu]["description"])
+        self.ui.menuStyles.setCurrentIndex(self.menuNames[self.selectedMenu]["menuIndex"])
 
         self.ui.menuStyles.activated.connect(self.setMenuStyle)
 
+    # Class properties
+    @property
+    def selectedMenu(self):
+        return self.__class__.screenSettings.get("selectedMenu", MENU_KICKOFF)
+
+    @selectedMenu.setter
+    def selectedMenu(self, value):
+        self.__class__.screenSettings["selectedMenu"] = value
+
+    # UI methods
     def setMenuStyle(self, enee):
         self.__class__.screenSettings["hasChanged"] = True
         currentIndex = self.ui.menuStyles.currentIndex()
 
         if currentIndex == 0:
-            self.__class__.screenSettings["selectedMenu"] = 'launcher'
-
-            self.ui.pictureMenuStyles.setPixmap(self.menuNames["launcher"]["image"])
-            self.ui.labelMenuDescription.setText(self.menuNames["launcher"]["description"])
+            self.setMenu(MENU_KICKOFF)
         elif currentIndex == 1:
-            self.__class__.screenSettings["selectedMenu"] = 'simplelauncher'
-
-            self.ui.pictureMenuStyles.setPixmap(self.menuNames["simplelauncher"]["image"])
-            self.ui.labelMenuDescription.setText(self.menuNames["simplelauncher"]["description"])
-
+            self.setMenu(MENU_KICKER)
         elif currentIndex == 2:
-            self.__class__.screenSettings["selectedMenu"] = 'lancelot_launcher'
+            self.setMenu(MENU_KICKERDASH)
 
-            self.ui.pictureMenuStyles.setPixmap(self.menuNames["lancelot_launcher"]["image"])
-            self.ui.labelMenuDescription.setText(self.menuNames["lancelot_launcher"]["description"])
-        elif currentIndex == 3:
-            self.__class__.screenSettings["selectedMenu"] = 'homerunlauncher'
+    def setMenu(self, menu):
+        self.selectedMenu = menu
 
-            self.ui.pictureMenuStyles.setPixmap(self.menuNames["homerunlauncher"]["image"])
-            self.ui.labelMenuDescription.setText(self.menuNames["homerunlauncher"]["description"])
-        elif currentIndex == 4:
-            self.__class__.screenSettings["selectedMenu"] = "appmenu-qml"
+        self.ui.pictureMenuStyles.setPixmap(self.menuNames[menu]["image"])
+        self.ui.labelMenuDescription.setText(self.menuNames[menu]["description"])
 
-            self.ui.pictureMenuStyles.setPixmap(self.menuNames["appmenu-qml"]["image"])
-            self.ui.labelMenuDescription.setText(self.menuNames["appmenu-qml"]["description"])
-        elif currentIndex == 5:
-            self.__class__.screenSettings["selectedMenu"] = 'org.kde.homerun-kicker'
-
-            self.ui.pictureMenuStyles.setPixmap(self.menuNames["org.kde.homerun-kicker"]["image"])
-            self.ui.labelMenuDescription.setText(self.menuNames["org.kde.homerun-kicker"]["description"])
-
+    # Screen methods
     def shown(self):
         pass
 
     def execute(self):
-        self.__class__.screenSettings["summaryMessage"] = self.menuNames[str(self.__class__.screenSettings["selectedMenu"])]["summaryMessage"]
+        self.__class__.screenSettings["summaryMessage"] = self.menuNames[self.selectedMenu]["summaryMessage"]
+
+        configFile = os.path.join(os.path.expanduser("~"), ".config",
+            "plasma-org.kde.plasma.desktop-appletsrc")
+        parser = Parser(configFile)
+        parser.setMenuStyleOrCreate(self.selectedMenu)
+
         return True
